3adbbd4d9d92087fa5797d7febed79854abe8f2e
Elvira Ramírez <elvirarp92@gmail.com>
2023-08-30 16:18:11 +0200

    Customize Vuetify theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -13,14 +13,24 @@ import { createVuetify } from 'vuetify'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
+  defaults: {
+    VBtn: {
+      class: 'text-none',
+    }
+  },
+
   theme: {
     themes: {
       light: {
         colors: {
-          primary: '#1867C0',
-          secondary: '#5CBBF6',
-        },
-      },
+          primary: '#E26D5A',
+          secondary: '#4DA1A9',
+          success: '#8FC93A',
+          warning: '#FFCC33',
+          error: '#E26D5A',
+          info: '#4DA1A9'
+        }
+      }
     },
   },
 })
