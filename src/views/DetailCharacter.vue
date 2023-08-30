1e16a5ed28a6c1d278276a6a86b7a05675b63a27
Elvira Ramírez <elvirarp92@gmail.com>
2023-08-30 21:21:47 +0200

    Include name and thumbnail on character detail

--- src/views/DetailCharacter.vue
+++ src/views/DetailCharacter.vue
@@ -1,19 +1,31 @@
 <template>
-  <article class="ma-8">
-    <h1 class="text-h1">
-      {{ character.name }}
-    </h1>
-  </article>
+  <v-container tag="article" class="ma-8">
+    <v-row>
+      <v-col cols="12" md="4">
+        <v-img height="350px" :src="thumbnail" />
+      </v-col>
+      <v-col>
+        <h1 class="text-h1">
+          {{ name }}
+        </h1>
+        <p>{{ description }}</p>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup>
 import { useRoute } from 'vue-router'
 import { getCharacter } from '@/api/index'
-import { ref, watch, onMounted } from 'vue'
+import { ref, computed, watch, onMounted } from 'vue'
 
 const route = useRoute()
 
-const character = ref({}) 
+/* Character variables */
+const character = ref({})
+const name = computed(() => character.value.name)
+const thumbnail = computed(() => `${character.value.thumbnail?.path}.${character.value.thumbnail?.extension}`)
+const description = computed(() => character.value.description)
 
 /* Watchers */
 watch(() => route.params.id, () => fetchCharacter() )
