44c77079b045cd6727df08bcb150289c5008527b
Elvira Ramírez <elvirarp92@gmail.com>
2023-08-30 18:48:27 +0200

    Render character list

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,13 +1,21 @@
 <template>
-  <h1>Hola Mundo</h1>
+  <section>
+    <p v-for="character in characters" :key="character.id">
+      {{ character.name }}
+    </p>
+  </section>
 </template>
 
 <script setup>
 import { getCharacters } from '@/api/index'
 import { ref, onMounted } from 'vue'
 
+const characters = ref([])
+
+/* Pagination variables */
 const page = ref(1)
 const itemsPerPage = ref(20)
+const totalCharacters = ref(0)
 
 onMounted(() => {
   const params = {
@@ -16,7 +24,10 @@ onMounted(() => {
   }
 
   getCharacters(params)
-    .then(res => console.log(res))
+    .then(res => {
+      characters.value = res.results
+      totalCharacters.value = res.total
+    })
     .catch(err => console.log(err))
 })
 </script>
